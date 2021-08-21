2f4c8b85ab02a0e189dd7ffdf8baa00730d0e998
Mel Michael <mel.michael007@gmail.com>
2021-08-20 21:03:14 -0700

    Present sample data in a table

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,51 @@
 import React from 'react';
+import { format } from 'date-fns';
+
 import './App.scss';
 
+import mockData from './data/accounts.json';
+
 function App() {
+  const columnheaders = [
+    'First Name',
+    'Last Name',
+    'Country Code',
+    'Email',
+    'Date of Birth',
+    'Tokens Held',
+    'Date Created'
+  ];
+
   return (
     <div className="container">
       <header className="my-4">
         <h1>Ledn Token Dashboard</h1>
       </header>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
+
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            {columnheaders.map((col) => (
+              <th key={col}>{col}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {mockData.map((data) => {
+            return (
+              <tr>
+                <th>{data['First Name']}</th>
+                <th>{data['Last Name']}</th>
+                <th>{data.Country}</th>
+                <th>{data.email.toLowerCase()}</th>
+                <th>{format(new Date(data.dob), 'MM-dd-yyyy')}</th>
+                <th>{data.amt.toLocaleString()}</th>
+                <th>{format(new Date(data.createdDate), 'MMM-dd-yyyy')}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
