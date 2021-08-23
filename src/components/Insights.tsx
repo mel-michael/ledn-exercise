087e94921d228eb282a32c24e93f871961b12f90
Mel Michael <mel.michael007@gmail.com>
2021-08-23 05:39:44 -0700

    Add one more insights to dashboard

--- src/components/Insights.tsx
+++ src/components/Insights.tsx
@@ -1,27 +1,38 @@
+import { useState, useEffect } from 'react';
+
 import '../App.scss';
+import { lednApi } from '../utils/api';
 
 type InsightProps = {
   countryCount: number;
   userCount: number;
 };
 
-export const Insights: React.FC<InsightProps> = ({ countryCount, userCount }) => (
-  <div className="d-flex justify-content-between border-top py-5">
-    <div className="border shadow-sm insights p-3 text-center">
-      <h5>Total Accounts</h5>
-      <h1>{userCount.toLocaleString()}</h1>
+export const Insights: React.FC<InsightProps> = ({ countryCount, userCount }) => {
+  const [tokenAmount, setTokenAmount] = useState(0);
+
+  useEffect(() => {
+    const findHighestToken = async () => {
+      const result = await lednApi.get('/highest-token');
+      setTokenAmount(result.data.account[0].amt);
+    };
+    findHighestToken();
+  }, []);
+
+  return (
+    <div className="d-flex justify-content-between border-top py-5">
+      <div className="border shadow-sm insights p-3 text-center">
+        <h5>Total Accounts</h5>
+        <h1>{userCount.toLocaleString()}</h1>
+      </div>
+      <div className="border shadow-sm insights p-3 text-center">
+        <h5>No. of Countries</h5>
+        <h1>{countryCount}</h1>
+      </div>
+      <div className="border shadow-sm insights p-3 text-center" style={{ width: 300 }}>
+        <h5>Highest Token Held</h5>
+        <h1>{tokenAmount.toLocaleString()}</h1>
+      </div>
     </div>
-    <div className="border shadow-sm insights p-3 text-center">
-      <h5>No. of Countries</h5>
-      <h1>{countryCount}</h1>
-    </div>
-    <div className="border shadow-sm insights p-3 text-center">
-      <h5>Total Accounts</h5>
-      <h1>{userCount.toLocaleString()}</h1>
-    </div>
-    <div className="border shadow-sm insights p-3 text-center">
-      <h5>No. of Countries</h5>
-      <h1>{countryCount}</h1>
-    </div>
-  </div>
-);
+  );
+};
